0c75489cf7c9e468a9e293daae485f0fffc68f3b
Zeeshan-Zahoor <shanmohd3939@gmail.com>
2026-01-18 21:06:46 +0530

    Lecture 04

--- 01Basics-vitereact/src/main.jsx
+++ 01Basics-vitereact/src/main.jsx
@@ -1,11 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import MoreChai from './MoreChai.jsx'
+import React from 'react'
+
+function MyApp() {
+  const anotherEl = " also click me"
+  return (
+    <>
+      <a href="https://google.com" target="_blank" ><br /> Click here to visit google website. {anotherEl}</a>
+    </>
+  )
+}
+
+const domElement = React.createElement(
+  'a', 
+  {href: 'https://google.com', target: '_blank'}, 
+  `click here to visit google website`
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {domElement}
+    <MyApp/>
     <MoreChai/>
   </StrictMode>,
 )
